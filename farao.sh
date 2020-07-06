35953747c780bdd258e4dccb07f258baeb833a7b
LughWalle <civalfran1@gmail.com>
2020-07-06 16:29:13 -0500

    adicionanco shellscript farao.sh

---
+++ farao.sh
@@ -0,0 +1,8 @@
+read -p "gostaria de baixar o video?? [s/n]" DECISAO
+if [ $DECISAO = "s" ]
+then 
+echo "Baixando"; sleep 1; echo .; sleep 1; echo .; sleep 1; echo .; sleep 1; echo .; sleep 1; echo .; sleep 1; echo .; sleep 1; echo .; sleep 1; echo .;
+echo "kkkkk nao tem video vai estudar"
+else
+echo "isso ai!! vai estudar"
+fi
